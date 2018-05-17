2d8f0b5675dc00984cd895bfc82b78e8f7d3d0e8
Lasse Kragh Sørensen <lasse.kragh.soerensen@gmail.com>
2018-05-17 14:41:14 +0200

    Test to tests

--- travis/run_travis_tests.sh
+++ travis/run_travis_tests.sh
@@ -2,9 +2,9 @@
 
 # run_travis_tests.sh: Run the tests for Travis CI.
 
-python -m test.test_pi_versions &&
-python -m test.test_init &&
-python -m test.test_sftp &&
-python -m test.test_run &&
-python -m test.test_status &&
-python -m test.test_delete 
+python -m tests.test_pi_versions &&
+python -m tests.test_init &&
+python -m tests.test_sftp &&
+python -m tests.test_run &&
+python -m tests.test_status &&
+python -m tests.test_delete 
