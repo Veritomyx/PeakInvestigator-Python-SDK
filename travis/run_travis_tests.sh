d4baf84da9dec24d69d56be751e20f7f6864b367
Lasse Kragh Sørensen <lasse.kragh.soerensen@gmail.com>
2018-05-21 08:52:58 +0200

    Inserting copyright statement

--- travis/run_travis_tests.sh
+++ travis/run_travis_tests.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
-
-# run_travis_tests.sh: Run the tests for Travis CI.
+## -*- coding: utf-8 -*-
+# 
+# Copyright (c) 2016, Veritomyx, Inc.
+#
+# This file is part of the Python SDK for PeakInvestigator
+# (http://veritomyx.com) and is distributed under the terms
+# of the BSD 3-Clause license.
 
 cd tests &&
 python -m test_pi_versions &&
