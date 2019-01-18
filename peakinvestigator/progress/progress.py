a0ee09a26034da6a597152e9d135ec48575af246
johncthomas738 <johncthomas77@gmail.com>
2019-01-18 14:16:23 -0800

    uploader_n_19jan18

---
+++ peakinvestigator/progress/progress.py
@@ -0,0 +1,25 @@
+## -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019, Veritomyx, Inc.
+#
+# This file is part of the Python SDK for PeakInvestigator
+# (http://veritomyx.com) and is distributed under the terms
+# of the BSD 3-Clause license.
+from abc import ABC, abstractmethod
+
+class ProgressFactory(ABC):
+    @abstractmethod
+    def create(self, total, unit):
+        """return an implementation of progress"""
+        print("Not implemented")
+
+class Progress(ABC):
+    @abstractmethod
+    def update(self, update):
+        """return an implementation of progress update"""
+        print("Not impelemnted")
+
+    @abstractmethod
+    def close(self):
+        """return an implementation of close progress"""
+        print("Not implemented")
