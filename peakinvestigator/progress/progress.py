fcc7985536e4c694f1e9e2ad5fb28c69920456f1
johncthomas738 <johncthomas77@gmail.com>
2019-01-22 10:19:41 -0800

    uploader_o_19jan22

--- peakinvestigator/progress/progress.py
+++ peakinvestigator/progress/progress.py
@@ -5,15 +5,18 @@
 # This file is part of the Python SDK for PeakInvestigator
 # (http://veritomyx.com) and is distributed under the terms
 # of the BSD 3-Clause license.
-from abc import ABC, abstractmethod
+from abc import ABCMeta, abstractmethod
+from six import add_metaclass
 
-class ProgressFactory(ABC):
+@add_metaclass(ABCMeta)
+class ProgressFactory():
     @abstractmethod
     def create(self, total, unit):
         """return an implementation of progress"""
         print("Not implemented")
 
-class Progress(ABC):
+@add_metaclass(ABCMeta)
+class Progress():
     @abstractmethod
     def update(self, update):
         """return an implementation of progress update"""
